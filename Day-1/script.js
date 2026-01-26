3f8ee583df3ad7969267cc243521261643d88d1c
Anuj5588 <ak4826636@gmail.com>
2026-01-26 20:57:17 +0530

    started the logic building from procodder

---
+++ Day-1/script.js
@@ -0,0 +1,147 @@
+
+// // solution- 1
+// const n = Number(prompt("Enter any natural Number:"))
+
+// var i
+// console.log(`the ${n}  natural number are :` )
+// for(i=1; i<=n; i++){
+  
+//     console.log(i)
+
+
+// }
+
+// var i
+
+// solution-2
+
+// for(i=1; i<=n; i++){
+// var rev = n-i +1;
+// console.log(rev)
+
+// }
+
+//solution -3
+
+// for(i=1; i<=n; i++){
+
+//     if(i%2 == 0){
+//         console.log(i)
+//     }
+
+// }
+
+// var sum =0;
+// //solution-4
+
+// for(i=1; i<=n; i++){
+
+//     sum = sum + i
+
+// }
+
+// console.log(sum)
+
+
+//solution:5
+// var fact = 1;
+
+// for(i=1; i<=n; i++){
+
+//     var fact =  (n-i+1)*fact
+   
+
+
+
+// }
+
+// console.log(fact)
+
+//solution-6
+// var sum =0;
+// for(i=1; i<=n; i++){
+
+//     if(i%2 == 0){
+//         sum = sum +i;
+      
+//     }
+
+// }
+// console.log(sum)
+
+
+// solution:7
+// var fact = 1
+// for(i=1; i<=n; i++){
+
+//      fact =  n-i+1
+//      fact = fact*fact
+
+//      console.log(fact)
+   
+
+// }
+
+
+
+
+//home work solution
+
+// solution-1
+
+
+// for(i=1; i<=n; i++){
+
+//     if(i%3==0 && i%5 == 0){
+//         console.log(i)
+//     }
+// }
+
+var sum =0
+//solution-2
+
+// for(i=1; i<=n; i++){
+//     if(i%2 !== 0){
+
+//         sum = sum + i
+
+//     }
+// }
+
+// console.log(sum)
+
+
+// solution-3
+
+// var fact = 1
+// for(i=1; i<=n; i++){
+
+//      fact =  n-i+1
+//      fact = fact*fact*fact
+
+//      console.log(fact)
+   
+
+// }
+
+
+
+// const start = Number(prompt("Enter first natural Number:"))
+
+// const end = Number(prompt("Enter last natural Number:"))
+
+// var result =""
+
+// for(i=start; i<=end; i++){
+//     if(i%2==0){
+//         let sqrt = Math.sqrt(i)
+
+//         if(Number.isInteger(sqrt)){
+//             result = result + i+" "
+//         }
+
+//     }
+ 
+// }
+
+//    console.log(result)
---
+++ Day-2/script.js
@@ -0,0 +1,45 @@
+//Solution-1
+
+
+//Find the Maximum of Three Numbers
+
+// const num1 = Number(prompt("Enter the first number"))
+// const num2 = Number(prompt("Enter the first number"))
+// const num3 = Number(prompt("Enter the first number"))
+
+// if(num1==num2 && num2==num3){
+
+//     console.log("All are equal")
+// }
+
+// else if(num1>num2 && num1>num3){
+//     console.log(`${num1} is greater`)
+// }
+
+// else if(num1>num2 && num1>num3){
+//     console.log(`${num2} is greater`)
+// }
+
+
+
+// else {
+// console.log(`${num3} is greater`)
+// }
+
+//solution-2
+
+//Check if a Number is Positive, Negative, or Zero
+const num = Number(prompt("Enter the  number"))
+
+if(num>0){
+    console.log("Postive")
+}
+
+else if(num<0){
+    console.log("Negative")
+}
+
+else{
+    console.log("Zero")
+}
+
