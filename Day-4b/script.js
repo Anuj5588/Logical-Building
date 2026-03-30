ca3cef21a27ff8bdfdbfccf73cc503815380be42
Anuj5588 <ak4826636@gmail.com>
2026-03-30 15:14:46 +0530

    working on day-4b

--- Day-4b/script.js
+++ Day-4b/script.js
@@ -27,26 +27,92 @@ let num = Number(prompt("Enter the number"));
 // }
 
 //**Check if a Number is an Armstrong Number (also called a Narcissistic Number)**
-let rev = 0;
-let temp = num
-let count =0
+// let rev = 0;
+// let temp = num
+// let count =0
 
-while (temp > 0) {
+// while (temp > 0) {
 
- temp = (temp-(temp%10))/10
-  count++
-}
-temp = num
-console.log(temp)
-while (temp > 0) {
-  let digit = temp % 10;
+//  temp = (temp-(temp%10))/10
+//   count++
+// }
+// temp = num
+// console.log(temp)
+// while (temp > 0) {
+//   let digit = temp % 10;
 
  
-  rev = rev * 1 + Math.pow(digit,count);
+//   rev = rev * 1 + Math.pow(digit,count);
 
-  temp = (temp-digit)/10
+//   temp = (temp-digit)/10
+// }
+
+// if(rev==num){
+// console.log("Armstrong Number")
+// }
+
+// **Find the Sum of Digits**
+
+// let sum =0
+
+// while(num>0){
+//   let temp = num%10
+//   sum = sum + temp
+//   num= (num-temp)/10
+
+// }
+
+// console.log(sum)
+
+//  **Find the Average of Digits**
+
+//  let avg = 0.0
+//   let sum=0.0
+//  while(num>0){
+ 
+//   let temp = num%10
+//   sum = sum + temp
+//   num = (num-temp)/10
+//    avg++
+
+
+//  }
+
+//  avg = sum/avg
+// console.log(avg)
+
+ //**Find the Largest and Smallest Digit in a Number**
+//  let greatenum=0
+//  let lownum=9;
+//  while(num>0){
+//     let temp = num%10
+//     if(temp>greatenum){
+//      greatenum = temp
+
+//     }
+    
+//     if(temp<lownum){
+//         lownum =temp
+//     }
+//     num = (num-temp)/10;
+// }
+
+ 
+// console.log(greatenum, "is greatest")
+// console.log(lownum, "is smallest")
+
+while(num>0){
+  
+     temp = 5
+     while(temp>0){
+     let fact = 1
+     fact = fact*temp
+     temp = temp-1
+     let fact1 = fact
+     console.log(fact1)
+     num = num-1
+     }
+
+
 }
 
-if(rev==num){
-console.log("Armstrong Number")
-}
